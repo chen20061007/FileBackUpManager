ad010f8ade0d443739c73ac616aa487bbd7849d2
chen20061007 <83200891+chen20061007@users.noreply.github.com>
2024-04-27 00:18:47 +0800

    test4/26

--- FileManager/main.py
+++ FileManager/main.py
@@ -17,68 +17,42 @@ path04 = r"E:\TestFile\test2\b"
 ########(位置,code)還是(位置,code)?
 ########雙for要用位置記還是code記?
 
-def is_hash_same(hash_dict1, hash_dict2):
+def hash_check(dict1, dict2):
     #整體一致性
-    a = list(hash_dict1.keys()) == list(hash_dict2.keys())
-    l1 = []
-    l2 = []
-    for path_src in hash_dict1.values():
-        path_suffix_src_list.append(path_src[len(in_path_src):])
-    for path_dest in hash_dict2.values():
-        path_suffix_dest_list.append(path_dest[len(in_path_dest):])
-    b = l1 == l2
+    a = (list(dict1.keys()) == list(dict2.keys()))
+    b = (list(dict1.values()) == list(dict2.values()))
     return a and b
 
 
 start_time = time.time()
 
 #輸入路徑
-in_path_src = path001
-in_path_dest = path02
+dir_in_src = path01
+dir_in_dest = path01
 
-#get文件全路徑+code的字典
-hash_dict_src = hash.get(in_path_src)#path:coed
-hash_dict_dest = hash.get(in_path_dest)#path:coed
-print(f"{hash_dict_dest}==={hash_dict_src}")
+hash_src = hash.hashData(dir_in_src)
+hash_dest = hash.hashData(dir_in_dest)
 
-
-hashcode_list_src = list(hash_dict_src.values())   #哈西列表
-path_list_src = list(hash_dict_src.keys())         #完整路徑列表
-
-hashcode_list_dest = list(hash_dict_dest.values()) #哈西列表
-path_list_dest = list(hash_dict_dest.keys())       #完整路徑列表
-
-path_suffix_src_list = []                          #後路徑列表
-path_suffix_dest_list = []                         #後路徑列表
-for path_src in path_list_src:
-    path_suffix_src_list.append(path_src[len(in_path_src)+1:])
-for path_dest in path_list_dest:
-    path_suffix_dest_list.append(path_dest[len(in_path_dest)+1:])
-
-print(f"{hashcode_list_src}==={hashcode_list_dest}")
-print(f"{path_list_src}==={path_list_dest}")
-print(f"{path_suffix_src_list}==={path_suffix_dest_list}")
-
-
-#1有2有 1有2沒有 1沒有2有
 copy_dict = {}
-index = 0
-overwrite = True
-for path_suffix_src in path_suffix_src_list:
-    if path_suffix_src in path_suffix_dest_list and hashcode_list_src[index] in hashcode_list_dest:
-        print(f"1有2有,原位置{path_suffix_src},一致,=>{hashcode_list_src[index]}")
-    elif path_suffix_src in path_suffix_dest_list:
-        print(f"1有2有,原位置{path_suffix_src},不一致,新內容=>{hashcode_list_src[index]}")
-        if overwrite:
-            copy_dict[path_list_src[index]] = os.path.join(in_path_dest, path_suffix_src)
-    else:
-        print(f"1有2沒有,原位置{path_suffix_src},新內容=>{hashcode_list_src[index]}")
-        copy_dict[path_list_src[index]] = os.path.join(in_path_dest, path_suffix_src)
-    index += 1
 
+is_same = hash_check(hash_src.dict, hash_dest.dict)
+if not is_same:
+    print("檔案不一致，進行檢查")
+    for i, subpath_src in enumerate(hash_src.subpath_list):
+        if subpath_src in hash_dest.subpath_list:  # 有同名檔案
+            if hash_src.code_list[i] in hash_dest.code_list:
+                # 內容相同
+                print("同檔案,同內容")
+            else:
+                # 內容不同
+                print("同檔案,不同內容")
+        else:
+            # 沒同名檔案
+            print("沒同檔案")
+else:
+    print("檔案一致")
 
 print(copy_dict)
-print(is_hash_same(hash_dict_src, hash_dict_dest))
 
 
 
